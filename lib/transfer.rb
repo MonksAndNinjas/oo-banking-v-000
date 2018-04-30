91dc3013977519a2968c64f34f6fec010c7a504b
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-04-30 00:13:10 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,7 +1,9 @@
 class Transfer
   # your code here
   def transfer(name_sender, name_receiver, money)
-    
+    @name_sender = name_sender
+    @name_receiver = name_reciever
+    @money = money
   end
 
 end
