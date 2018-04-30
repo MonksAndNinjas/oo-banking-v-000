cac6a5157ac40809c71460e5a81fdc1f72020cf2
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-04-30 00:16:08 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,10 +1,12 @@
 class Transfer
-  attr_reader :name_sender, :namer_receiver
+  attr_accessor :name_sender, :namer_receiver
   # your code here
   def initialize(name_sender, name_receiver, money)
     @name_sender = name_sender
     @name_receiver = name_receiver
     @money = money
   end
+
+
 
 end
